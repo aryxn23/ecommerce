1860b7e48e75766dadec8f0186515da06b806409
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-10-01 23:31:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -186,11 +186,11 @@ app.post('/create-checkout-session', async (req, res) => {
         mode: 'payment',
         success_url: "https://ecommerce-deploy-lft5.vercel.app/success",
         cancel_url: "https://ecommerce-deploy-lft5.vercel.app",
-         metadata={
+         metadata:{
     "payment_type": "schedule_visit",
     "visit_id": "123"
   },
-  payment_intent_data={
+  payment_intent_data:{
     "metadata": {
       "payment_type": "schedule_visit",
       "visit_id": "123"
